42d62918ea987b328b93c61ed7af10676e6f3dfa
ronald_uribe <ronald_uribe@hotmail.com>
2024-08-05 12:53:33 +0200

    Initial commit

---
+++ src/main/java/com/exagonal/tasks/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.exagonal.tasks.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    
+	@Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("ronalduribef@gmail.com");
+        contact.setName("Ruribe");
+        contact.setUrl("https://github.com/ronalduribe/arquitectura_hexagonal/");
+
+        Info info = new Info()
+                .title("Tutorial Management API by ruribe")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage tutorials.");
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
+    
+}
--- src/main/java/com/exagonal/tasks/infrastructure/entities/TaskEntity.java
+++ src/main/java/com/exagonal/tasks/infrastructure/entities/TaskEntity.java
@@ -1,12 +1,12 @@
 package com.exagonal.tasks.infrastructure.entities;
 
-import com.exagonal.tasks.domain.model.Task;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+
+import com.exagonal.tasks.domain.model.Task;
 
 @Entity
 public class TaskEntity {
